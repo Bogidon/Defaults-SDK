ae0c06a639323b11155b0077e1da1461c57909de
Bogdan Vitoc <bogdanvitoc@gmail.com>
2015-09-04 23:33:37 -0400

    Added project

---
+++ Defaults-SDK/Defaults-SDK.h
@@ -0,0 +1,19 @@
+//
+//  Defaults-SDK.h
+//  Defaults-SDK
+//
+//  Created by Bogdan Vitoc on 9/4/15.
+//  Copyright (c) 2015 Defaults. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Defaults-SDK.
+FOUNDATION_EXPORT double Defaults_SDKVersionNumber;
+
+//! Project version string for Defaults-SDK.
+FOUNDATION_EXPORT const unsigned char Defaults_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Defaults_SDK/PublicHeader.h>
+
+
